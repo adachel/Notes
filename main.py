f201b0b1ff588a889db0d8a0a57a169cf7529b2a
adach <adache74@gmail.com>
2024-10-21 18:11:33 +0500

    from work 21.10

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import datetime
 def Create_or_Add_Note(name_note, text_note): # создать или добавить заметку
     db = datetime.datetime.now()
     current_time = db.strftime('%m.%d.%Y %H:%M')
-
     try:
         with open('data.json', encoding= 'utf-8') as file:
             file_content = file.read()
@@ -14,20 +13,19 @@ def Create_or_Add_Note(name_note, text_note): # создать или добав
             note = {"Id": count + 1, "time": current_time, "name": name_note, "text": text_note}
             templates.append(note)
 
-        with open('data.json', 'w', encoding= 'utf-8') as file:
+        with open('data.json', 'w', encoding='utf-8') as file:
             for i in range(len(templates)):
                 if i == 0:
-                    file.writelines(f'[{json.dumps(templates[i])},\n')
+                    file.write(f'[{json.dumps(templates[i], ensure_ascii=False)},\n')
                 elif i == len(templates) - 1:
-                    file.writelines(f'{json.dumps(templates[i])}]\n')
+                    file.write(f'{json.dumps(templates[i], ensure_ascii=False)}]\n')
                 else:
-                    file.writelines(f'{json.dumps(templates[i])},\n')
-
-
+                    file.write(f'{json.dumps(templates[i], ensure_ascii=False)},\n')
     except:
         new_note = [{"Id": 1, "time": current_time, "name": name_note, "text": text_note}]
-        with open('data.json', 'w', encoding= 'utf-8') as file:
-            file.write(json.dumps(new_note))
+        with open('data.json', 'w', encoding='utf-8') as file:
+            file.write(json.dumps(new_note, ensure_ascii=False))
+
 
 def List_Note(): # список заметоk
     with open('data.json', encoding= 'utf-8') as file:
@@ -35,37 +33,46 @@ def List_Note(): # список заметоk
         templates = json.loads(file_content)
 
     for i in range(len(templates)):
-        print(f"Id: {templates[i]["Id"]}; время: {templates[i]["time"]}; наименование: {templates[i]["name"]}")
+        print(f"Id: {templates[i]["Id"]}; время записи: {templates[i]["time"]}; наименование: {templates[i]["name"]}")
 
 
 def Edit_Note(Id = 0, name = ""):
-    with open('data.json', encoding='utf-8') as file:
-        file_content = file.read()
-        templates = json.loads(file_content)
+    try:
+        with open('data.json', encoding='utf-8') as file:
+            file_content = file.read()
+            templates = json.loads(file_content)
 
-    for i in range(len(templates)):
-        if(Id == templates[i]["Id"] or name == templates[i]["name"]):
-            text = templates[i]["text"]
-            print(f"Текущий текст\n{text}")
-            print("Введите новый текст")
-            new_text = input()
-            templates[i]["text"] = new_text
-            with open('data.json', 'w', encoding='utf-8') as file:
-                json.dump(templates, file, ensure_ascii=False)
+        for i in range(len(templates)):
+            if(Id == templates[i]["Id"] or name == templates[i]["name"]):
+                text = templates[i]["text"]
+                print(f"Текущий текст\n{text}")
+                print("Введите новый текст")
+                new_text = input()
+                templates[i]["text"] = new_text
+                with open('data.json', 'w', encoding='utf-8') as file:
+                    for i in range(len(templates)):
+                        if i == 0:
+                            file.write(f'[{json.dumps(templates[i], ensure_ascii=False)},\n')
+                        elif i == len(templates) - 1:
+                            file.write(f'{json.dumps(templates[i], ensure_ascii=False)}]\n')
+                        else:
+                            file.write(f'{json.dumps(templates[i], ensure_ascii=False)},\n')
+    except:
+        print("Заметок нет")
 
 
 
 
 
 
-name_note = input("Название заметки\n")
-text_note = input("Текст заметки\n")
-
-Create_or_Add_Note(name_note, text_note)
+# name_note = input("Название заметки\n")
+# text_note = input("Текст заметки\n")
+#
+# Create_or_Add_Note(name_note, text_note)
 
 # List_Note()
 
-# Edit_Note(0, "первая")
+Edit_Note(0, "первая")
 
 
 
